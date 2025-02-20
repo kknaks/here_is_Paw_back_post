38ecd7168c09105a1969457385ada6cc2ba7eba4
이건학 <kknaks@igeonhag-ui-MacBookAir.local>
2025-02-20 12:18:51 +0900

    Feat: init

---
+++ hereIsPaw/src/main/java/com/ll/hereispaw/HereIsPawApplication.java
@@ -0,0 +1,13 @@
+package com.ll.hereispaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereIsPawApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HereIsPawApplication.class, args);
+  }
+
+}
---
+++ hereIsPaw/src/test/java/com/ll/hereispaw/HereIsPawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.hereispaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereIsPawApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
