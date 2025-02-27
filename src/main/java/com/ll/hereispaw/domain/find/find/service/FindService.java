e709c0197459bbaa77524560c5055d8ee05615ba
이건학 <kknaks@igeonhag-ui-MacBookAir.local>
2025-02-27 22:18:26 +0900

    merge:

--- src/main/java/com/ll/hereispaw/domain/find/find/service/FindService.java
+++ src/main/java/com/ll/hereispaw/domain/find/find/service/FindService.java
@@ -66,6 +66,7 @@ public class FindService {
             .image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZAHxgFBNiIPBx5mXMtIoMN7udx45JgJOoq0aZqnhHhxcQEKsjcjMivntAgRt_tUIP8ZsY5wOuNyXscwNWrBoHYGZfhFzhCPAO2lq87Ag")
             .postType(POST_TYPE)
             .postId(savedPost.getId())
+            .postMemberId(savedPost.getMember_id())
             .build();
         kafkaTemplate.send("dog-face-request", dogFaceRequestDto);
         return savedPost.getId(); // 저장된 find_post_id 반환
