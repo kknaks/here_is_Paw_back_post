dea29658b5ee42830d2f0906b049dc88de8f778f
이건학 <kknaks@igeonhag-ui-MacBookAir.local>
2025-02-28 09:36:38 +0900

    Deploy: 8try

--- src/main/java/com/ll/hereispaw/global/config/AppConfig.java
+++ src/main/java/com/ll/hereispaw/global/config/AppConfig.java
@@ -7,19 +7,8 @@ public class AppConfig {
     @Getter
     private static String siteFrontUrl;
 
-    @Getter
-    private static String devFrontUrl;
-
     @Value("${custom.site.frontUrl}")
     public void setSiteFrontUrl(String siteFrontUrl) {
         AppConfig.siteFrontUrl = siteFrontUrl;
-    }
-
-    @Value("${custom.dev.frontUrl}")
-    public void setDevFrontUrl(String devFrontUrl) {
-        AppConfig.devFrontUrl = devFrontUrl;
-    }
-    public static boolean isNotProd() {
-        return true;
     }
 }
